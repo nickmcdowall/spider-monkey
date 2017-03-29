ed905f7e9cbc77902474f9364169e307cd687aac
nick mcdowall <nicholas.mcdowall@gmail.com>
2017-03-29 22:07:40 +0100

    Add sample integration test for running application

---
+++ src/test/java/com/nick/intrastructure/HelpControllerIntegrationTest.java
@@ -0,0 +1,25 @@
+package com.nick.intrastructure;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class HelpControllerIntegrationTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void exampleTest() {
+        String body = this.restTemplate.getForObject("/help", String.class);
+        assertThat(body).contains("<h1>Help!</h1>");
+    }
+}
